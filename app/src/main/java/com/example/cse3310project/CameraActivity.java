be6899df3ac32465aa9c7df51a0f813f1acff9b9
Colby Wyrick <buttergeneral@outlook.com>
2022-03-23 16:37:23 -0500

    Added CameraActivity and activity_camera.xml, and gave permissions in manifest. Will work on after exam tomorrow.

---
+++ app/src/main/java/com/example/cse3310project/CameraActivity.java
@@ -0,0 +1,21 @@
+package com.example.cse3310project;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CameraActivity extends AppCompatActivity
+{
+
+}
